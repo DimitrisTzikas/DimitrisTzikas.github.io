b60e5429cbc2b315f42f5665b317c5ce25229c2d
Dimitris Tzikas <dimitristzikas@protonmail.com>
2018-07-22 20:20:10 +0300

    07-22-2018

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// Used to add scrollbar on small screens
+var element = document.querySelector('#portfolio');
+var element1 = document.querySelector('#check-scroll');
+if( (element.offsetHeight < element.scrollHeight) || (element.offsetWidth < element.scrollWidth)){
+  // Your element have overflow
+  element1.removeAttribute('id')
+  element1.setAttribute("id", "scroll");
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleMenu() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+    
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  }
+);
